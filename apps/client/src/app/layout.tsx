1f9a4d3c0b9bdc27dca220a4f955939613a01d8e
dohpark <dohpark1129@gmail.com>
2023-07-15 12:38:47 +0900

    chore: #4 nextjs 환경 설정 및 폴더 구조 변경

---
+++ apps/client/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "BoostForm",
+  description: "Create Your Own Form",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
