30a73450d2f169c38b333f34b714dc92b350e713
dohpark <dohpark1129@gmail.com>
2023-07-15 23:29:53 +0900

    #5 chore: 로그인 페이지 styled-components에서 tailwind css로 적용

--- apps/client/src/pages/Login/index.tsx
+++ apps/client/src/pages/Login/index.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import * as S from "./style";
 
 function Login() {
   const router = useRouter();
@@ -17,17 +16,25 @@ function Login() {
   };
 
   return (
-    <S.Container>
-      <S.LoginContainer>
+    <section className="w-full h-screen flex items-center justify-center select-none bg-blue3">
+      <div className="flex flex-col items-center justify-center w-[600px] pt-20 pb-14 border border-grey3 rounded-lg bg-white">
         <Image src="/icons/github.svg" height={16} width={16} alt="github icon" />
         <Image src="/icons/logo.svg" width={100} height={50} alt="logo" draggable={false} />
-        <S.OAuthButton onClick={handleClickOAuth}>
+        <button
+          onClick={handleClickOAuth}
+          className="w-[460px] flex items-center justify-center bg-black border-none text-white mt-9 py-2 px-4 text-base rounded-lg cursor-pointer font-medium"
+        >
           <Image src="/icons/github.svg" height={16} width={16} alt="github icon" />
-          <S.ButtonText>깃허브 로그인</S.ButtonText>
-        </S.OAuthButton>
-        <S.HomeButton onClick={handleClickHome}>메인 화면으로 돌아가기</S.HomeButton>
-      </S.LoginContainer>
-    </S.Container>
+          <span className="ml-2">깃허브 로그인</span>
+        </button>
+        <button
+          onClick={handleClickHome}
+          className="w-[460px] bg-grey2 border-none text-black mt-4 py-2 px-4 text-base rounded-lg cursor-pointer font-medium"
+        >
+          메인 화면으로 돌아가기
+        </button>
+      </div>
+    </section>
   );
 }
 
