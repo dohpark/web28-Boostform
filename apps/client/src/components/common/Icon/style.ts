88e0f7f4897ff9e4532ca4c5dc9d39bc542a0b1a
dohpark <dohpark1129@gmail.com>
2023-07-17 20:36:32 +0900

    remove: icon 컴포넌트 삭제

--- apps/client/src/components/common/Icon/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Container = styled.span<{ size: string }>`
-  display: inline-block;
-  height: ${({ size }) => size};
-  width: ${({ size }) => size};
-`;
-
-export default Container;
