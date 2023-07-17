d7713c0010f20f0ae063deefaba7f5686756bcb2
dohpark <dohpark1129@gmail.com>
2023-07-17 16:35:32 +0900

    chore: #5 Notice 컴포넌트 tailwindcss로 수정

--- apps/client/src/components/common/Notice/index.tsx
+++ apps/client/src/components/common/Notice/index.tsx
@@ -1,29 +1,10 @@
 import React from "react";
-import theme from "styles/theme";
-import Container from "./style";
 
-function Notice({
-  text,
-  color = theme.colors.blue3,
-  backgroundColor = theme.colors.white,
-  border = theme.colors.blue3,
-}: {
-  text: string;
-  color?: string;
-  backgroundColor?: string;
-  border?: string;
-}) {
-  return (
-    <Container color={color} backgroundColor={backgroundColor} border={border}>
-      {text}
-    </Container>
-  );
+function Notice({ text, className: customCss }: { text: string; className: string }) {
+  const defaultCss = "my-4 mx-0 py-4 px-0 text-center font-medium ";
+  const className = defaultCss + customCss;
+
+  return <p className={className}>{text}</p>;
 }
-
-Notice.defaultProps = {
-  color: theme.colors.blue3,
-  backgroundColor: theme.colors.white,
-  border: theme.colors.blue3,
-};
 
 export default Notice;
