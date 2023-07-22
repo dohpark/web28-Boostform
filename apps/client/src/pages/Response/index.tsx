f23ac467b272ffe91dd79e34ac724e2f52592cdd
dohpark <dohpark1129@gmail.com>
2023-07-23 07:10:05 +0900

    refactor: #5 Response 페이지 tailwindcss로 수정

--- apps/client/src/pages/Response/index.tsx
+++ apps/client/src/pages/Response/index.tsx
@@ -1,22 +1,24 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
-import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
-import formApi from "api/formApi";
-import Skeleton from "components/common/Skeleton";
-import FormLayout from "components/template/Layout";
-import useLoadingDelay from "hooks/useLoadingDelay";
-import { FormDataApi } from "types/form";
-import * as S from "./style";
+import formApi from "@/api/formApi";
+import Skeleton from "@/components/common/Skeleton";
+import FormLayout from "@/components/template/Layout";
+import useLoadingDelay from "@/hooks/useLoadingDelay";
+import { FormDataApi } from "@/types/form";
+import { useParams, useRouter } from "next/navigation";
+import Link from "next/link";
 
 function Result() {
   const { id } = useParams();
-  const { state } = useLocation() as {
-    state: { responseId: string; type: "submitResponse" | "duplicateResponse" | "endResponse" };
-  };
-  const navigate = useNavigate();
+  // const { state } = useLocation() as {
+  //   state: { responseId: string; type: "submitResponse" | "duplicateResponse" | "endResponse" };
+  // };
+  const router = useRouter();
 
-  const fetchForm = (): Promise<FormDataApi> => formApi.getForm(id);
+  const fetchForm = (): Promise<FormDataApi> => formApi.getForm(id as string);
   const { data, isSuccess, isLoading } = useQuery({
     queryKey: [id, "form"],
     queryFn: fetchForm,
@@ -35,19 +37,21 @@ function Result() {
   }, [isSuccess, data, id]);
 
   const onClickModifyPreviousResponse = () => {
-    navigate(`/forms/${id}/view`, { state: state.responseId });
+    router.push(`/forms/${id}/view`, {
+      // state: state.responseId
+    });
   };
 
   const onClickNavigateOtherResponse = () => {
-    navigate(`/forms/${id}/view`);
+    router.push(`/forms/${id}/view`);
   };
 
   const getTitle = () => {
-    if (state && state.type === "submitResponse") return "응답이 기록되었습니다.";
-    if (state && state.type === "duplicateResponse") return "이미 응답했습니다.";
-    if (state && state.type === "endResponse") return "더 이상 응답을 받지 않습니다.";
-    navigate("/");
-    return "";
+    // if (state && state.type === "submitResponse") return "응답이 기록되었습니다.";
+    // if (state && state.type === "duplicateResponse") return "이미 응답했습니다.";
+    // if (state && state.type === "endResponse") return "더 이상 응답을 받지 않습니다.";
+    // router.push("/");
+    return "test";
   };
 
   const checkApiSuccess = () => {
@@ -61,8 +65,8 @@ function Result() {
 
   return (
     <FormLayout backgroundColor="blue">
-      <S.Container>
-        <S.ResponseWrapper>
+      <div className="w-[760px]">
+        <div className="mt-9 bg-white rounded pt-2 px-5 pb-7">
           {checkApiLoadingOrError() ? (
             <>
               <Skeleton.Element type="formTitle" />
@@ -73,18 +77,34 @@ function Result() {
           ) : null}
           {checkApiSuccess() ? (
             <>
-              <S.Title>{form?.title}</S.Title>
-              <S.Description>{getTitle()}</S.Description>
+              <div className="w-full block text-3xl py-1 border-none leading-[48px]">{form?.title}</div>
+              <p className="pt-2 text-sm font-normal">{getTitle()}</p>
               {form?.acceptResponse ? (
-                <S.LinkWrapper>
-                  {form?.responseModifiable ? <S.Link onClick={onClickModifyPreviousResponse}>응답 수정</S.Link> : null}
-                  {!form?.loginRequired ? <S.Link onClick={onClickNavigateOtherResponse}>다른 응답 제출</S.Link> : null}
-                </S.LinkWrapper>
+                <div className="mt-6 flex flex-col">
+                  {form?.responseModifiable ? (
+                    <Link
+                      className="pt-1 text-sm font-normal text-blue4 underline cursor-pointer"
+                      href={`/forms/${id}/view`}
+                      // onClick={onClickModifyPreviousResponse}
+                    >
+                      응답 수정
+                    </Link>
+                  ) : null}
+                  {!form?.loginRequired ? (
+                    <Link
+                      className="pt-1 text-sm font-normal text-blue4 underline cursor-pointer"
+                      href={`/forms/${id}/view`}
+                      // onClick={onClickNavigateOtherResponse}
+                    >
+                      다른 응답 제출
+                    </Link>
+                  ) : null}
+                </div>
               ) : null}
             </>
           ) : null}
-        </S.ResponseWrapper>
-      </S.Container>
+        </div>
+      </div>
     </FormLayout>
   );
 }
