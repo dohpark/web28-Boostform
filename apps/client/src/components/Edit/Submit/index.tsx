0eac8f94c2111d77b8967a077b70a40031e45f4a
dohpark <dohpark1129@gmail.com>
2023-07-24 19:12:15 +0900

    refactor: #10 edit submit 상태관리 추가

--- apps/client/src/components/Edit/Submit/index.tsx
+++ apps/client/src/components/Edit/Submit/index.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/common/Button";
 
-function Submit() {
+function Submit({ openModal }: { openModal: () => void }) {
   return (
     <div className="flex justify-end mt-4 mb-8 mx-0 bg-white rounded p-5 relative overflow-hidden">
       <Button type="button" onClick={() => openModal()} className="bg-blue5 border bordery-grey3 text-white">
