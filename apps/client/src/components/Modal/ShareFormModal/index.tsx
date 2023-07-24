944a19dbba89ad7a370f40dd1fe3a2911971424e
dohpark <dohpark1129@gmail.com>
2023-07-24 19:13:03 +0900

    refactor: #10 ShareFormModal 상태관리 추가

--- apps/client/src/components/Modal/ShareFormModal/index.tsx
+++ apps/client/src/components/Modal/ShareFormModal/index.tsx
@@ -4,18 +4,89 @@ import Button from "@/components/common/Button";
 import Chain from "@public/icons/chain.svg";
 import ShareFormModalProps from "./type";
 import COLORS from "@/constants/color";
+import { useFormStore } from "@/store/edit";
+import { fromFormToApi } from "@/utils/form";
+import { toast } from "react-toastify";
+import formApi from "@/api/formApi";
+import { useParams } from "next/navigation";
 
-function ShareFormModal({
-  formState,
-  closeModal,
-  changeLoginRequired,
-  changeOnBoardShare,
-  changeAcceptResponse,
-  changeResponseModifiable,
-  saveForm,
-  copyLink,
-}: ShareFormModalProps) {
-  const { loginRequired, onBoard, acceptResponse, responseModifiable } = formState;
+function ShareFormModal({ closeModal }: ShareFormModalProps) {
+  const { id } = useParams();
+  const { form, question, actions: formActions } = useFormStore();
+  const { loginRequired, onBoard, acceptResponse, responseModifiable } = form;
+
+  const onClickChangeLoginRequired = () => {
+    formActions.changeLoginRequired();
+  };
+
+  const onClickChangeOnBoardShare = () => {
+    formActions.changeOnBoardShared();
+  };
+
+  const onClickChangeAcceptResponse = () => {
+    formActions.changeAcceptResponse();
+  };
+
+  const onClickChangeResponseModifiable = () => {
+    formActions.changeResponseModifiable();
+  };
+
+  const onClickCopyLink = () => {
+    window.navigator.clipboard.writeText(`${"127.0.0.1:3000"}/forms/${id}/view`);
+    toast.success("링크가 복사되었습니다!", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+
+  const saveForm = () => {
+    if (!id) return;
+    if (!form.title) {
+      toast.error("제목을 작성해주세요!", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      return;
+    }
+    if (!form.category) {
+      toast.error("카테고리를 정해주세요!", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      return;
+    }
+    const apiData = fromFormToApi({ form, question });
+    formApi.saveForm(id as string, apiData);
+    toast.success("저장이 완료되었습니다.!", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+
   const onClickCancel = () => closeModal();
   const onClickSave = () => {
     saveForm();
@@ -28,23 +99,23 @@ function ShareFormModal({
       <div>
         <div className="flex justify-between items-center mt-3">
           <span>응답 받기</span>
-          <ToggleButton state={acceptResponse} onClick={changeAcceptResponse} />
+          <ToggleButton state={acceptResponse} onClick={onClickChangeAcceptResponse} />
         </div>
         <div className="flex justify-between items-center mt-3">
           <span>응답횟수 1회로 제한 (로그인 필수)</span>
-          <ToggleButton state={loginRequired} onClick={changeLoginRequired} />
+          <ToggleButton state={loginRequired} onClick={onClickChangeLoginRequired} />
         </div>
         <div className="flex justify-between items-center mt-3">
           <span>응답 수정 가능</span>
-          <ToggleButton state={responseModifiable} onClick={changeResponseModifiable} />
+          <ToggleButton state={responseModifiable} onClick={onClickChangeResponseModifiable} />
         </div>
         <div className="flex justify-between items-center mt-3">
           <span>게시판에 공유하기</span>
-          <ToggleButton state={onBoard} onClick={changeOnBoardShare} />
+          <ToggleButton state={onBoard} onClick={onClickChangeOnBoardShare} />
         </div>
         <div className="flex justify-between items-center mt-3">
           <span>링크</span>
-          <Button type="button" onClick={copyLink} className="border-none bg-transparent text-xs p-0">
+          <Button type="button" onClick={onClickCopyLink} className="border-none bg-transparent text-xs p-0">
             <Chain fill={COLORS.blue3} height="24" width="24" />
             <span className="text-blue4 ml-2">링크 복사하기</span>
           </Button>
--- apps/client/src/components/Modal/ShareFormModal/type.ts
+++ apps/client/src/components/Modal/ShareFormModal/type.ts
@@ -1,14 +1,5 @@
-import { FormSummary } from "@/types/form";
-
 interface ShareFormModalProps {
-  formState: FormSummary;
   closeModal: () => void;
-  changeLoginRequired: () => void;
-  changeOnBoardShare: () => void;
-  changeAcceptResponse: () => void;
-  changeResponseModifiable: () => void;
-  saveForm: () => void;
-  copyLink: () => void;
 }
 
 export default ShareFormModalProps;
