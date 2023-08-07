88c1a11ead74c22bb7e254479576afaf94c9c3e6
dohpark <dohpark1129@gmail.com>
2023-08-07 13:29:45 +0900

    refactor: #14 csr 설정

--- apps/client/src/app/forms/[id]/edit/page.tsx
+++ apps/client/src/app/forms/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import Loading from "./loading";
-import Edit from "@/components/Edit";
+import dynamic from "next/dynamic";
+const Edit = dynamic(() => import("@/components/Edit"), { ssr: false, loading: () => <Loading /> });
 
 export default async function Page() {
   return (
